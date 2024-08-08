33e16e1c671f74c34811be4fb3d97b06e25e6c1a
Mohammed Master <mohammedmaster1793@gmail.com>
2024-08-08 13:30:09 +0100

    initial variables

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// starter variables
+let numTimer = document.querySelector(".numtimer");
+let startBtn = document.querySelector(".starting");
+let pauseBtn = document.querySelector(".pause");
+let restBtn = document.querySelector(".reset");
+let workDuration = document.querySelector(".work-duration");
+let breakDuration = document.querySelector(".break-duration");
