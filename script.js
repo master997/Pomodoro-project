e5b5659c0046d2431d8af222736d080ecdf74854
Mohammed Master <mohammedmaster1793@gmail.com>
2024-08-08 13:41:20 +0100

    some more stuff

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ let numTimer = document.querySelector(".numtimer");
 let startBtn = document.querySelector(".starting");
 let pauseBtn = document.querySelector(".pause");
 let restBtn = document.querySelector(".reset");
-let workDuration = document.querySelector(".work-duration");
-let breakDuration = document.querySelector(".break-duration");
+let workDurationInput = document.querySelector(".work-duration");
+let breakDurationInput = document.querySelector(".break-duration");
+
+let intervalId;
+let remainingTime;
+let isRunning = false;
+let isWorkSession = true;
+let workDuration = 1500;
+let breakDuration = 300;
