94b1b07f05f4305f7a27a8d2cb8f86f61ec86ac5
Mohammed Master <mohammedmaster1793@gmail.com>
2024-09-07 21:02:55 +0100

    work duration input stuff

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const breakDurInput = document.querySelector(".break-duration");
 
 // Event listeners
 startBtn.addEventListener("click", function () {
+  // puts input into number then checks if input is a number or 0. then changes it to 25
+  let workDurValue = Number(workDurInput.value);
+  if (isNaN(workDurValue) || workDurValue === 0) {
+    workDurValue = 25;
+  }
+  // converting to seconds is easier in js because its easier to decrease by 1 sec every interval tick
+  let minToSecs = Number(workDurValue * 60);
+  console.log(minToSecs);
   console.log("start button is clicked");
 });
 
